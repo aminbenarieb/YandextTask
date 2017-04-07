e9e8a7aaf6f5236ae78f0adcc52e75b06488c375
Amin Benarieb <amin.benarieb.work@gmail.com>
2017-04-07 21:35:52 +0300

    Added basic translate view.

---
+++ app/src/main/java/com/aminbenarieb/yandextask/HomeTranslateFragment.java
@@ -0,0 +1,69 @@
+package com.aminbenarieb.yandextask;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by aminbenarieb on 4/6/17.
+ */
+
+public class HomeTranslateFragment extends Fragment {
+    private View mContent;
+    private TextView mTranslateSourceTextView;
+    private TextView mTranslateResultTextView;
+
+
+    public HomeTranslateFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mContent = inflater.inflate(R.layout.translate_view, container, false);
+        return mContent;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupViews(view);
+        setupViewsAppearance();
+        setupLayout();
+        populateViews();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    //region Setup subroutines
+    void setupViews(View view) {
+        mContent = view.findViewById(R.id.translate_view);
+        mTranslateSourceTextView = (TextView) view.findViewById(R.id.translate_source);
+        mTranslateResultTextView = (TextView) view.findViewById(R.id.translate_result);
+    }
+
+    void setupViewsAppearance() {
+
+    }
+
+    void setupLayout() {
+
+    }
+    //endregion
+
+    void populateViews() {
+        mTranslateSourceTextView.setText("Hello");
+
+        mTranslateResultTextView.setText(Html.fromHtml("<b>Здравствуйте</b>"));
+    }
+
+}

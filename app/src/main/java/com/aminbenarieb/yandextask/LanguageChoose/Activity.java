ba1a94522a196b8b4ec5b751f6dd9fef430b86fd
Amin Benarieb <amin.benarieb.work@gmail.com>
2017-04-09 20:37:23 +0300

    Language chooser acitivity.

---
+++ app/src/main/java/com/aminbenarieb/yandextask/LanguageChoose/Activity.java
@@ -0,0 +1,41 @@
+package com.aminbenarieb.yandextask.LanguageChoose;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+import com.aminbenarieb.yandextask.R;
+
+import java.util.List;
+
+/**
+ * Created by aminbenarieb on 4/9/17.
+ */
+
+public class Activity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private List<String> mLanguageList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_language_recycleview);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_language_chooser);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mLanguageList = getIntent().getStringArrayListExtra("LANGUAGES");
+        if (mAdapter == null)
+        {
+            mAdapter = new Adapter();
+            mRecyclerView.setAdapter(mAdapter);
+            return;
+        }
+
+        mAdapter.notifyDataSetChanged();
+    }
+}
